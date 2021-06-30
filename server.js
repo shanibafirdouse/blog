836e203e729e7e2e23a1bbeb5a25afc20f63d2e0
shaniba firdouse <shanibafirdouse22@gmail.com>
2021-07-01 01:38:44 +0530

    incomplete mongoose connection

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express=require('express')
+const mongoose=require('mongoose')
 const articleRouter=require('./routes/article')
 const app=express()
-
+  
+mongoose.connect('mongodb://localhost/blog',{
+    useNewUrlParser:true,useUnifiedTopology:true
+})
 
 app.set('view engine','ejs')
 
